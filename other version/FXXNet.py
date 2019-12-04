7930d645329e1fdce93f2030e39e48e73ccb400f
zxdclyz <1515685918@qq.com>
2019-12-04 21:17:40 +0800

    Add report, clean up

--- other version/FXXNet.py
+++ other version/FXXNet.py
@@ -1,3 +1,4 @@
+# search "path to" and fix it
 import argparse
 import os
 import random
@@ -20,7 +21,8 @@ import torchvision.datasets as datasets
 import torchvision.models as models
 import torchvision.transforms as transforms
 from PIL import Image
-import pretrainedmodels
+import imgaug as ia
+from imgaug import augmenters as iaa
 
 # parser
 model_names = sorted(name for name in models.__dict__
@@ -30,7 +32,7 @@ model_names = sorted(name for name in models.__dict__
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 
 parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
-                    #choices=model_names,
+                    choices=model_names,
                     help='model architecture: ' +
                     ' | '.join(model_names) +
                     ' (default: resnet18)')
@@ -84,7 +86,7 @@ def main_worker(gpu, ngpus_per_node, args):
 
     # create model
     print("=> creating model '{}'".format(args.arch))
-    model = pretrainedmodels.__dict__[args.arch](pretrained='imagenet')
+    model = models.__dict__[args.arch](pretrained=True)
 
 #     model.aux_logits=False
 
@@ -110,7 +112,6 @@ def main_worker(gpu, ngpus_per_node, args):
     cudnn.benchmark = True
 
     # set dataset
-    # set dataset
     train_dataset_transformed = TinyImageNetDataset(
         './TinyImageNet', './TinyImageNet/train.txt',
         transform=transforms.Compose([
@@ -123,6 +124,7 @@ def main_worker(gpu, ngpus_per_node, args):
     train_dataset_original = TinyImageNetDataset(
         './TinyImageNet', './TinyImageNet/train.txt',
         transform=transforms.ToTensor())
+    
     val_dataset = TinyImageNetDataset(
         './TinyImageNet', './TinyImageNet/val.txt', transform=transforms.ToTensor())
     test_dataset = TinyImageNetDataset(
@@ -393,6 +395,73 @@ def accuracy(output, target, topk=(1,)):
             correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
+
+# Defining Customized Imagedatagenerator using imgaug library
+
+
+def CustomImageDataGen(input_img):
+    # Sometimes(0.5, ...) applies the given augmenter in 50% of all cases,
+    # e.g. Sometimes(0.5, GaussianBlur(0.3)) would blur roughly every second
+    # image.
+    def sometimes(aug): return iaa.Sometimes(0.5, aug)
+
+    seq = iaa.Sequential([
+        iaa.Fliplr(0.5),  # horizontal flips
+        iaa.Flipud(0.2),  # vertical flips
+
+        # Small gaussian blur with random sigma between 0 and 0.5.
+        # But we only blur about 50% of all images.
+        sometimes(iaa.GaussianBlur(sigma=(0, 2.0))),
+
+        # crop images by -10% to 20% of their height/width
+        sometimes(iaa.CropAndPad(
+            percent=(-0.1, 0.2),
+            pad_mode=ia.ALL,
+            pad_cval=(0, 255)
+        )),
+
+        # Apply affine transformations to some of the images
+        # - scale to 80-120% of image height/width (each axis independently)
+        # - translate by -20 to +20 relative to height/width (per axis)
+        # - rotate by -45 to +45 degrees
+        # - shear by -16 to +16 degrees
+        # - order: use nearest neighbour or bilinear interpolation (fast)
+        # - mode: use any available mode to fill newly created pixels
+        #         see API or scikit-image for which modes are available
+        # - cval: if the mode is constant, then use a random brightness
+        #         for the newly created pixels (e.g. sometimes black,
+        #         sometimes white)
+        sometimes(iaa.Affine(
+            scale={"x": (0.8, 1.5), "y": (0.8, 1.5)},
+            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+            rotate=(-45, 45),
+            shear=(-16, 16),
+            order=[0, 1],
+            cval=(0, 255),
+            mode=ia.ALL
+        )),
+
+        # drop 2-5% percent of the original size, leading to large dropped
+        # rectangles.
+        sometimes(iaa.CoarseDropout(
+            (0.03, 0.15), size_percent=(0.02, 0.05),
+            per_channel=0.2
+        )),
+
+        # Make some images brighter and some darker.
+        # In 20% of all cases, we sample the multiplier once per channel,
+        # which can end up changing the color of the images.
+        sometimes(iaa.Multiply((0.8, 1.2), per_channel=0.2)),
+
+        # Improve or worsen the contrast of images.
+        # Comment it out after third model run (extreme saturation)
+        sometimes(iaa.ContrastNormalization((0.75, 1.5), per_channel=0.5)),
+    ],
+        # do all of the above augmentations in random order
+        random_order=True)  # apply augmenters in random order
+
+    output_img = seq.augment_image(input_img)
+    return output_img
 
 
 if __name__ == '__main__':
